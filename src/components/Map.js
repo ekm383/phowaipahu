31317d9218fb393c334820a1779efb005831637e
Eric Manongdo <eric@superscriptcreative.com>
2020-08-07 11:18:33 -1000

    First Commit

---
+++ src/components/Map.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const Map = () => {
+  return (
+    <div>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3715.232285558236!2d-158.02095974893302!3d21.380756585739142!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7c00653b3e6d1027%3A0xacc7c13f6e54b90!2sPho%20Waipahu!5e0!3m2!1sen!2sus!4v1596834587429!5m2!1sen!2sus"
+        width="100%"
+        height="400"
+        frameborder="0"
+        allowfullscreen=""
+        aria-hidden="false"
+        tabindex="0"
+      ></iframe>
+    </div>
+  )
+}
+export default Map
